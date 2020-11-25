c09e4c91cb1136e46639ad80feb18148ac382e5f
Truonghdpk <truongondaihoc@gmail.com>
2020-11-25 12:27:17 +0700

    add style

--- packages/extensions/products-slider-module/src/components/Sider/Slider.js
+++ packages/extensions/products-slider-module/src/components/Sider/Slider.js
@@ -1,22 +1,65 @@
 import React from 'react';
 import Swiper from 'react-id-swiper';
+import "./slider-style.css";
 const Slider = () => {
-  const params = {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    }
-  }
-  return (
-    <Swiper {...params}>
-      <div>Slide #1</div>
-      <div>Slide #2</div>
-      <div>Slide #3</div>
-      <div>Slide #4</div>
-      <div>Slide #5</div>
-    </Swiper>
-  )
+    const params = {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        }
+    };
+    return (
+        <div className="product-slider-container">
+            <Swiper {...params}>
+                <div
+                    style={{
+                        width: '300px',
+                        height: '400px',
+                        backgroundColor: 'yellow'
+                    }}
+                >
+                    Slide #1
+                </div>
+                <div
+                    style={{
+                        width: '300px',
+                        height: '400px',
+                        backgroundColor: 'yellow'
+                    }}
+                >
+                    Slide #1
+                </div>
+                <div
+                    style={{
+                        width: '300px',
+                        height: '400px',
+                        backgroundColor: 'yellow'
+                    }}
+                >
+                    Slide #1
+                </div>
+                <div
+                    style={{
+                        width: '300px',
+                        height: '400px',
+                        backgroundColor: 'yellow'
+                    }}
+                >
+                    Slide #1
+                </div>
+                <div
+                    style={{
+                        width: '300px',
+                        height: '400px',
+                        backgroundColor: 'yellow'
+                    }}
+                >
+                    Slide #1
+                </div>
+            </Swiper>
+        </div>
+    );
 };
 export default Slider;
