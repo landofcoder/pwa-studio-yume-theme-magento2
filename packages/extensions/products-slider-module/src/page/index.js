fd732955b8700bd43150e577b4ad07a7683cfe92
Truong Nguyen <truongkyle@chxnhungs-Mini.localdomain>
2020-11-26 17:39:36 +0700

    add commit style

--- packages/extensions/products-slider-module/src/page/index.js
+++ packages/extensions/products-slider-module/src/page/index.js
@@ -4,7 +4,21 @@ import styles from './index.css';
 const App = () => {
     return (
         <div>
-            <div className={styles.title}>Top products</div>
+            <div className={styles.title}>
+                <h2>Top products</h2>
+            </div>
+            <Slider />
+            <div className={styles.title}>
+                <h2>Feature products</h2>
+            </div>
+            <Slider />
+            <div className={styles.title}>
+                <h2>Best seller products</h2>
+            </div>
+            <Slider />
+            <div className={styles.title}>
+                <h2>Deals products</h2>
+            </div>
             <Slider />
         </div>
     );
