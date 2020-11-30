974dd6956c25d968216a7c94ee3f08f2de45abf2
Truonghdpk <truongondaihoc@gmail.com>
2020-11-30 10:22:31 +0700

    add banner

--- packages/extensions/products-slider-module/src/components/Banner/Banner.js
+++ packages/extensions/products-slider-module/src/components/Banner/Banner.js
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React  from 'react';
 import Swiper from 'react-id-swiper';
 import styles from './style.css';
-import { useQuery } from '@apollo/client';
 
-import sliderQuery from './Banner.gql';
-// import Product from "@magento/venia-ui/lib/components/Gallery";
-import GalleryItem from '@landofcoder/yume-ui/src/components/Gallery/item';
 
 const mapGalleryItem = item => {
     const { small_image } = item;
@@ -17,25 +13,34 @@ const mapGalleryItem = item => {
 };
 
 const Slider = () => {
-    const { queries } = sliderQuery;
-    const { getTopProductQuery } = queries;
-    const { data, error, loading } = useQuery(getTopProductQuery);
-    console.log(data);
     const params = {
         slidesPerView: 1,
-        spaceBetween: 30
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          }
     };
-    if (loading) return null;
-    const galleryItems = data.products.items.map((item, index) => {
-        return (
-            <div>
-                <GalleryItem key={index} item={mapGalleryItem(item)} />
-            </div>
-        );
-    });
     return (
         <div className={styles.bannerStyle}>
-            <Swiper {...params}>{galleryItems}</Swiper>
+            <Swiper {...params}>
+                <div>
+                    <img
+                        className={styles.imageStyle}
+                        src={
+                            'https://venia.magento.com/media/venia-hero2.jpg?auto=webp&format=pjpg&quality=85'
+                        }
+                    />
+                </div>
+                <div>
+                    <img
+                        className={styles.imageStyle}
+                        src={
+                            'https://venia.magento.com/media/venia-hero1.jpg?auto=webp&format=pjpg&quality=85'
+                        }
+                    />
+                </div>
+            </Swiper>
         </div>
     );
 };
