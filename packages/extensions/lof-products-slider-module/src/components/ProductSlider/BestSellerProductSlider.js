a863f6067f9c35db3d5c1df8c068bbc4d13c1db2
HaNguyenQuang <nguyenquangha0209@gmail.com>
2021-02-01 15:30:23 +0700

    update code product slider

--- packages/extensions/lof-products-slider-module/src/components/ProductSlider/BestSellerProductSlider.js
+++ packages/extensions/lof-products-slider-module/src/components/ProductSlider/BestSellerProductSlider.js
@@ -20,7 +20,8 @@ const BestSellerProductSlider = () => {
     const { queries } = sliderQuery;
     const { getBestSellerProductQuery } = queries;
     const { data, error, loading } = useQuery(getBestSellerProductQuery);
-    console.log(data);
+    console.log("data best", data);
+    console.log("error", error);
     const params = {
         slidesPerView: 5,
         spaceBetween: 30,
@@ -42,18 +43,26 @@ const BestSellerProductSlider = () => {
             }
         }
     };
-    if (loading) return null;
-    const galleryItems = data.lofProductListBestseller.items.map((item, index) => {
+    if (loading || data === undefined){
+        return (<div/>)
+    } else {
+        const galleryItems = data.lofProductListBestseller.items.map((item, index) => {
+            return (
+                <div key={index}>
+                    <GalleryItem key={index} item={mapGalleryItem(item)} />
+                </div>
+            );
+        });
         return (
-            <div key={index}>
-                <GalleryItem key={index} item={mapGalleryItem(item)} />
+            <div>
+                <div className={styles.title}>
+                    <h2>Best seller products</h2>
+                </div>
+                <div className="product-slider-container">
+                    <Swiper {...params}>{galleryItems}</Swiper>
+                </div>
             </div>
         );
-    });
-    return (
-        <div className="product-slider-container">
-            <Swiper {...params}>{galleryItems}</Swiper>
-        </div>
-    );
+    }
 };
 export default BestSellerProductSlider;
